f55c802f94bc50e8f189901968a322be4e9ec9b4
iiiristram <iiiristram@gmail.com>
2014-08-20 18:59:15 +0400

    UnitOfWork destructor fix

--- Domain/Implementation/UnitOfWork.cs
+++ Domain/Implementation/UnitOfWork.cs
@@ -64,7 +64,6 @@ namespace Domain.Implementation
 
         public void Dispose()
         {
-            GC.SuppressFinalize(this);
             if (!_disposed)
             {
                 // наверняка завершем трансакцию
@@ -73,6 +72,7 @@ namespace Domain.Implementation
             }
 
             _disposed = true;
+            GC.SuppressFinalize(this);
         }
 
         private DbContext Context
